12788080078383330856d24bd67ff6e4b2bf459e
erikleisinger <erikleisinger@gmail.com>
2020-09-23 17:45:26 +0000

    upon successfully connecting to the server, the client is notified of connection and sends initials 'EL'

--- client.js
+++ client.js
@@ -10,10 +10,18 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf-8');
 
+  //upon connection to the server, we're notified of successful connection
+  conn.on('connect', () => {
+    console.log('You have connected to the server');
+    conn.write('Name: EL');
+
+  });
+
+  // when we receive data from the server, we log that to the console.
   conn.on('data', (message) => {
     console.log(message);
   });
-
+  // when we receive an error from the server, it's logged to the console.
   conn.on('error', (message) => {
     console.log(message);
   });
