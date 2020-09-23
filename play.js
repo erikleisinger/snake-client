5f438287be3a4c616b63a3bb550d2f2f2670c6b9
erikleisinger <erikleisinger@gmail.com>
2020-09-23 18:13:06 +0000

    third .js created to store setupInput(). Exported and imported by play.js

--- play.js
+++ play.js
@@ -1,21 +1,8 @@
 const connect = require('./client');
+const setupInput = require('./input');
 
 
 
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-
-  return stdin;
-};
 
 
 console.log('Connecting...');
